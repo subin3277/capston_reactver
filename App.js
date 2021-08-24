f5d802ceac640ca56bb06228fb302de07e6bebe5
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-08-24 23:37:38 +0900

    layout

--- App.js
+++ App.js
@@ -7,15 +7,24 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+    </View>,
+    <View style={styles.onlytext}>
+      <Text>오늘도 건강하세요</Text>
+      <Text>출산 예정일</Text>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  onlytext:{
+    backgroundColor:'#fff',
+    alignItems:'center'
+  }
+  
 });
